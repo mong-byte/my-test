edc8ede69d1d6ccf4d1add6e02d3bd31f4480f26
MINGI KIM <tkg78912@gmail.com>
2023-06-09 16:38:39 +0900

    feat: change style

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex p-24">
      this is graphite test
         <div>
             add graphite test
