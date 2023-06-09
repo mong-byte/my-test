808c734eb9ad77ae0cb73e578be49a19f24b72f2
MINGI KIM <tkg78912@gmail.com>
2023-06-09 11:14:56 +0900

    feat: delete p tag
    feat: deleted code

--- app/page.tsx
+++ app/page.tsx
@@ -52,9 +52,6 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
         </a>
 
         <a
@@ -63,15 +60,6 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
         </a>
       </div>
     </main>
