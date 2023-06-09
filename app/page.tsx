cba81151d2a98619ba4c615390e1e9fa043bbc05
MINGI KIM <tkg78912@gmail.com>
2023-06-09 11:16:14 +0900

    feat: deleted

--- app/page.tsx
+++ app/page.tsx
@@ -46,12 +46,6 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
         </a>
         
       </div>
