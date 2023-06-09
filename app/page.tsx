1b4249157c705a856921c2721dce358cd48a2290
MINGI KIM <tkg78912@gmail.com>
2023-06-09 14:28:20 +0900

    feat: change text

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      this is graphite test
+        <div>
+            add graphite test
+        </div>
     </main>
   )
 }
